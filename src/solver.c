5bd3b5425b0d000f17668f53dd6ed6e31adff0ea
Juan Lopez <jc.lopez11@uniandes.edu.co>
2017-03-22 09:27:00 -0500

    Perfection, only one more function

--- src/solver.c
+++ src/solver.c
@@ -1,1 +1,219 @@
-#include "solver.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+
+//Transform function for accessing the U-indices
+int transform_U(U_grid *U, int pos_x, int pos_y, int pos_z, int prop)
+{
+	return ((pos_x*U->N_x + pos_y)*U->N_y + pos_z)*U->N_z + prop;
+}
+
+//Extracts the value of rho for a given cell
+FLOAT extract_rho(U_grid *U, int pos_x, int pos_y, int pos_z)
+{
+	return U->U[transform_U(U,pos_x,pos_y,pos_z,0)];
+}
+
+//Extracts the value of u for a given cell
+FLOAT extract_u(U_grid *U, int pos_x, int pos_y, int pos_z)
+{
+	return U->U[transform_U(U,pos_x,pos_y,pos_z,1)]/extract_rho(U, pos_x, pos_y, pos_z);
+}
+
+//Extracts the value of v for a given cell
+FLOAT extract_v(U_grid *U, int pos_x, int pos_y, int pos_z)
+{
+	return U->U[transform_U(U,pos_x,pos_y,pos_z,2)]/extract_rho(U, pos_x, pos_y, pos_z);
+}
+
+//Extracts the value of w for a given cell
+FLOAT extract_w(U_grid *U, int pos_x, int pos_y, int pos_z)
+{
+	return U->U[transform_U(U,pos_x,pos_y,pos_z,3)]/extract_rho(U, pos_x, pos_y, pos_z);
+}
+
+//Extracts the value of E for a given cell
+FLOAT extract_E(U_grid *U, int pos_x, int pos_y, int pos_z)
+{
+	return U->U[transform_U(U,pos_x,pos_y,pos_z,4)]/extract_rho(U, pos_x, pos_y, pos_z);
+}
+
+//Calculates the value of e
+FLOAT calce(FLOAT E, FLOAT u, FLOAT v, FLOAT w)
+{
+	return E - 0.5*(u*u+v*v+w*w);
+}
+
+//Calculates the value of p
+FLOAT calcp(FLOAT rho, FLOAT e)
+{
+	return rho*e*(GAMMA-1);
+}
+
+//Calculates h
+FLOAT calch(FLOAT E, FLOAT p, FLOAT rho)
+{
+	return E + p/rho;
+}
+
+//Calculates cs
+FLOAT calcs(FLOAT h)
+{
+	return sqrt(h*(GAMMA+1));
+}
+
+/*TODO
+ * Find the general max value of (u+cs,v+cs,w+cs) of all cells
+ * This is necessary for calculating the maximum sound speed*/
+
+//Finds the maximum sounds speed of the whole array
+FLOAT calcsps(U_grid *U, FLOAT cs)
+{
+	return 0;
+}
+
+//Calculates dt
+FLOAT calcdt(physics_grid *P, FLOAT sps)
+{
+	return 0.5*P->delta_x/sps;
+}
+
+//Transform function for accessing the F-indices
+int transform_F(F_grid *F, int pos_x, int pos_y, int pos_z, int pos_g, int prop)
+{
+	return (((pos_x*F->N_x + pos_y)*F->N_y + pos_z)*F->N_z + pos_g)*NDIM + prop;
+}
+
+//Calculate F-vector
+void calcF(F_grid *F, U_grid *U, int pos_x, int pos_y, int pos_z)
+{
+	FLOAT rho,u,v,w,E,e,p;
+	rho = extract_rho(U,pos_x,pos_y,pos_z);
+	u = extract_u(U,pos_x,pos_y,pos_z);
+	v = extract_v(U,pos_x,pos_y,pos_z);
+	w = extract_w(U,pos_x,pos_y,pos_z);
+	E = extract_E(U,pos_x,pos_y,pos_z);
+	e = calce(E,u,v,w);
+	p = calcp(rho,e);
+	F->F[transform_F(F,pos_x,pos_y,pos_z,0,0)] = rho*u;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,1,0)] = rho*v;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,2,0)] = rho*w;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,0,1)] = rho*u*u+p;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,1,1)] = rho*u*v;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,2,1)] = rho*u*w;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,0,2)] = rho*u*v;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,1,2)] = rho*v*v+p;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,2,2)] = rho*v*w;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,0,3)] = rho*u*w;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,1,3)] = rho*v*w;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,2,3)] = rho*w*w+p;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,0,4)] = rho*(E+p/rho)*u;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,1,4)] = rho*(E+p/rho)*v;
+	F->F[transform_F(F,pos_x,pos_y,pos_z,2,4)] = rho*(E+p/rho)*w;
+}
+
+	//FLOAT h,cs,sps,dt; To be used in iterations
+	//h = calch(E,p,rho);
+	//cs = calcs(h);
+	//sps = calcsps(U,cs);
+
+//One iteration for U-update
+void newU(U_grid *U, F_grid *F, physics_grid *P, int pos_x, int pos_y, int pos_z, FLOAT dt)
+{
+	FLOAT *F_nhjk,*F_phjk,*F_inhk,*F_iphk,*F_ijnh,*F_ijph;
+	F_nhjk = (FLOAT *)malloc((NDIM+2)*sizeof(FLOAT));
+	F_phjk = (FLOAT *)malloc((NDIM+2)*sizeof(FLOAT));
+	F_inhk = (FLOAT *)malloc((NDIM+2)*sizeof(FLOAT));
+	F_iphk = (FLOAT *)malloc((NDIM+2)*sizeof(FLOAT));
+	F_ijnh = (FLOAT *)malloc((NDIM+2)*sizeof(FLOAT));
+	F_ijph = (FLOAT *)malloc((NDIM+2)*sizeof(FLOAT));
+	int i;
+	if(pos_x > 0 && pos_x < F->N_x)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_nhjk[i] = (-F->F[transform_F(F,pos_x+1,pos_y,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x-1,pos_y,pos_z,0,i)]);
+			F_phjk[i] = (-F->F[transform_F(F,pos_x-1,pos_y,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x+1,pos_y,pos_z,0,i)]);
+		}
+	}
+	else if(pos_x == 0)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_nhjk[i] = (-F->F[transform_F(F,pos_x+1,pos_y,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]);
+			F_phjk[i] = (6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x+1,pos_y,pos_z,0,i)]);
+		}
+	}
+	else if(pos_x == F->N_x)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_nhjk[i] = (6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x-1,pos_y,pos_z,0,i)]);
+			F_phjk[i] = (-F->F[transform_F(F,pos_x-1,pos_y,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]);
+		}
+	}
+	
+	if(pos_y > 0 && pos_y < F->N_y)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_inhk[i] = (-F->F[transform_F(F,pos_x,pos_y+1,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y-1,pos_z,0,i)]);
+			F_iphk[i] = (-F->F[transform_F(F,pos_x,pos_y-1,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y+1,pos_z,0,i)]);
+		}
+	}
+	else if(pos_y == 0)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_inhk[i] = (-F->F[transform_F(F,pos_x,pos_y+1,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]);
+			F_iphk[i] = (6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y+1,pos_z,0,i)]);
+		}
+	}
+	else if(pos_y == F->N_y)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_inhk[i] = (6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y-1,pos_z,0,i)]);
+			F_iphk[i] = (-F->F[transform_F(F,pos_x,pos_y-1,pos_z,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]);
+		}
+	}
+	
+	if(pos_z > 0 && pos_z < F->N_z)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_ijnh[i] = (-F->F[transform_F(F,pos_x,pos_y,pos_z+1,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y,pos_z-1,0,i)]);
+			F_ijph[i] = (-F->F[transform_F(F,pos_x,pos_y,pos_z-1,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y,pos_z+1,0,i)]);
+		}
+	}
+	else if(pos_z == 0)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_ijnh[i] = (-F->F[transform_F(F,pos_x,pos_y,pos_z+1,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]);
+			F_ijph[i] = (6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y,pos_z+1,0,i)]);
+		}
+	}
+	else if(pos_z == F->N_z)
+	{
+		for(i=0;i<NDIM+2;i++)
+		{
+			F_ijnh[i] = (6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]+3*F->F[transform_F(F,pos_x,pos_y,pos_z-1,0,i)]);
+			F_ijph[i] = (-F->F[transform_F(F,pos_x,pos_y,pos_z-1,0,i)]+6*F->F[transform_F(F,pos_x,pos_y,pos_z,0,i)]);
+		}
+	}
+	
+	for(i=0;i<NDIM+2;i++)
+	{
+		U->U[transform_U(U,pos_x,pos_y,pos_z,i)] += dt*(F_nhjk[i]+F_inhk[i]+F_ijnh[i]-F_phjk[i]-F_iphk[i]-F_ijph[i])/P->delta_x;
+	}
+	
+	free(F_nhjk);
+	free(F_phjk);
+	free(F_inhk);
+	free(F_iphk);
+	free(F_ijnh);
+	free(F_ijph);
+}
